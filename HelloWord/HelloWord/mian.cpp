37b7027866555bb17bca97c45e43773138eb3d5b
DESKTOP-DMBJUVJ\gavin <gavin6116@gmail.com>
2017-03-08 17:57:03 +0800

    第一个C++项目

---
+++ HelloWord/HelloWord/mian.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Hello,\n world!" << endl;
+
+	int a;
+
+	cout << "ÇëÊäÈë" << endl;
+
+	cin >> a;
+	a = a + 2;
+
+	cout << "Now you have " << a << " a "<<endl;
+
+
+	
+	return a;
+}
