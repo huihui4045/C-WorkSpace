8e64ca8ca4754639d69662efe1a09b589fe88fbf
DESKTOP-DMBJUVJ\gavin <gavin6116@gmail.com>
2017-08-03 16:07:14 +0800

    C语言demo

---
+++ CDemo/CDemo/CDemo.cpp
@@ -0,0 +1,45 @@
+// CDemo.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+
+
+void demo() {
+
+	float weight;
+
+	float value;
+
+	printf("请输入你的黄金的体重：");
+
+	scanf("%f",&weight);
+
+	value = weight * 1000 * 14.5811;
+
+
+	printf("结果：%.3f \n",value);
+}
+
+void demo2() {
+	char ch = 'C';
+
+	printf("zifu:%c \n",ch);
+	printf("zifu:%d\n", ch);
+
+}
+
+int main()
+{
+	//int num;
+	//num = 1;
+	//printf("jieguo:%d",num);
+
+	//demo();
+
+	demo2();
+
+	system("pause");
+    return 0;
+}
+
