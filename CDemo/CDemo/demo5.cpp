03335d62b77fa01a3b1925ef34fd94bf5046386b
DESKTOP-DMBJUVJ\gavin <gavin6116@gmail.com>
2017-08-09 18:12:38 +0800

    C语言运算符

--- CDemo/CDemo/demo5.cpp
+++ CDemo/CDemo/demo5.cpp
@@ -1,8 +1,7 @@
 #include "stdafx.h"
-#include <string.h>
 
-int main() {
 
+void demo() {
 
 	int n = 4;
 
@@ -10,9 +9,34 @@ int main() {
 
 	intsize = sizeof(int);
 
-	printf("n的字节大小：%zd, sizeof n=:%zd   intsize=%zd\n",n,sizeof n,intsize);
+	printf("n的字节大小：%zd, sizeof n=:%zd   intsize=%zd\n", n, sizeof n, intsize);
 
-	printf("自增运算符：%d\n",(2+ n++)*6);
+	printf("自增运算符：%d\n", (2 + n++) * 6);
+}
+
+void pound(int n) {
+	while (n-- >0)
+	{
+		printf("#");
+		
+	}
+	printf("\n");
+}
+
+int main() {
+
+	int times = 5;
+	char ch = '!';
+	float f = 8.0;
+
+	pound(times);
+	pound(ch);
+
+	pound(f);
+
+
+	//demo();
+	
 
 	system("pause");
 	return 0;
